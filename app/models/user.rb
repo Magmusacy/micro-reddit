69ab41ccb4b08d7079489f759ddb3d219a3b63ae
Magmusacy <xpfk.098@gmail.com>
2021-11-28 14:08:46 +0100

    Add Comment model with validations and Post model with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: { message: 'can not be omitted' }, length: { in: 6..12 }, uniqueness: true
+
+  has_many :posts
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_28_104810) do
+ActiveRecord::Schema.define(version: 2021_11_28_115232) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.text "body"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
